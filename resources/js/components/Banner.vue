1ea8cf9214bc504ef6d92e639ffb082cb2e71dd7
duong8888 <duongna16032003@gmail.com>
2024-05-22 23:16:18 +0700

    update banner 2

--- resources/js/components/Banner.vue
+++ resources/js/components/Banner.vue
@@ -2,25 +2,6 @@
     <!-- Banner Section Start -->
     <div class=" style5 h-50">
         <div class="container pt-100">
-<!--            <div id="carouselExample" class="carousel slide">-->
-<!--                <div class="carousel-inner">-->
-<!--                    <div class="carousel-item active">-->
-<!--                        <img src="@/assets/images/common/banner.jpg" alt="logo">-->
-<!--                    </div>-->
-<!--                    <div class="carousel-item">-->
-<!--                        <img src="@/assets/images/common/banner.jpg" alt="logo">-->
-<!--                    </div>-->
-<!--                    <div class="carousel-item">-->
-<!--                        <img src="@/assets/images/common/icon.jpg" alt="logo">-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev" fdprocessedid="al7imx">-->
-<!--                    <span class="carousel-control-prev-icon"></span>-->
-<!--                </button>-->
-<!--                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next" fdprocessedid="baw31v">-->
-<!--                    <span class="carousel-control-next-icon"></span>-->
-<!--                </button>-->
-<!--            </div>-->
             <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
                 <div class="carousel-inner">
                     <div class="carousel-item" data-bs-interval="10000">
