896472837fc8a0793109ff1e3de37d354da28c05
duong8888 <duongna16032003@gmail.com>
2024-05-27 12:28:02 +0700

    build js

--- resources/js/views/admin/CreatePosts.vue
+++ resources/js/views/admin/CreatePosts.vue
@@ -1,32 +1,32 @@
 <template>
     <div class="container">
-        <ckeditor :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
+<!--        <ckeditor :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>-->
     </div>
 </template>
 
 <script>
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+// import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 export default {
     name: "CreatePosts",
     data() {
         return {
-            editor: ClassicEditor,
-            editorData: '',
-            editorConfig: {
-                plugins: [
-                    ...ClassicEditor.builtinPlugins,
-                ],
-                toolbar: [
-                    'heading', '|',
-                    'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote',
-                    'imageUpload', 'mediaEmbed', '|',
-                    'insertTable', 'tableColumn', 'tableRow', 'mergeTableCells', '|',
-                    'undo', 'redo',
-                ],
-                ckfinder: {
-                    uploadUrl: '/api/upload-image',
-                },
-            }
+            // editor: ClassicEditor,
+            // editorData: '',
+            // editorConfig: {
+            //     plugins: [
+            //         ...ClassicEditor.builtinPlugins,
+            //     ],
+            //     toolbar: [
+            //         'heading', '|',
+            //         'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote',
+            //         'imageUpload', 'mediaEmbed', '|',
+            //         'insertTable', 'tableColumn', 'tableRow', 'mergeTableCells', '|',
+            //         'undo', 'redo',
+            //     ],
+            //     ckfinder: {
+            //         uploadUrl: '/api/upload-image',
+            //     },
+            // }
         };
     },
 }
