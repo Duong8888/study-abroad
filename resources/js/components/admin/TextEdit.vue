b67315cf521570474547b2a4279c5d5a5f661a98
duong8888 <duongna16032003@gmail.com>
2024-05-27 12:36:07 +0700

    build js 5/27

--- resources/js/components/admin/TextEdit.vue
+++ resources/js/components/admin/TextEdit.vue
@@ -10,48 +10,48 @@
 
 <script>
 // import '../../assets/summernote/jquery/jquery-3.4.1.slim.min.js';
-import '../../assets/summernote/summernote-lite.min.js';
-import '../../assets/summernote/lang/summernote-vi-VN.min';
-import '../../assets/summernote/summernote-lite.min.css';
+// import '../../assets/summernote/summernote-lite.min.js';
+// import '../../assets/summernote/lang/summernote-vi-VN.min';
+// import '../../assets/summernote/summernote-lite.min.css';
 
 export default {
     mounted() {
-        this.edit();
+        // this.edit();
     },
     methods: {
-        edit() {
-            $(this.$refs.summernote).summernote({
-                toolbar: [
-                    ['style', ['style']],
-                    ['font', ['bold', 'underline', 'clear', 'italic', 'fontname']],
-                    ['color', ['color']],
-                    ['fontsize', ['fontsize']],
-                    ['para', ['ul', 'ol', 'paragraph']],
-                    ['table', ['table']],
-                    ['insert', ['link', 'picture', 'video']],
-                    ['view', ['codeview', 'help',]],
-                    ['hr', ['hr']],
-                    ['undo', ['undo']],
-                    ['redo', ['redo']],
-                ],
-                fontNames: ['Arial', 'Arial Black', 'Comic Sans MS', 'Courier New', 'Merriweather', 'Times New Roman'],
-                fontNamesIgnoreCheck: ['Merriweather'],
-                lang: 'vi-VN',
-                callbacks: {
-                    onChange: (contents) => {
-                        this.$emit('content', contents);
-                    }
-                }
-            });
-        },
-        // save() {
-        //     var markup = $(this.$refs.summernote).summernote('code');
-        //     $(this.$refs.summernote).summernote('destroy');
+        // edit() {
+        //     $(this.$refs.summernote).summernote({
+        //         toolbar: [
+        //             ['style', ['style']],
+        //             ['font', ['bold', 'underline', 'clear', 'italic', 'fontname']],
+        //             ['color', ['color']],
+        //             ['fontsize', ['fontsize']],
+        //             ['para', ['ul', 'ol', 'paragraph']],
+        //             ['table', ['table']],
+        //             ['insert', ['link', 'picture', 'video']],
+        //             ['view', ['codeview', 'help',]],
+        //             ['hr', ['hr']],
+        //             ['undo', ['undo']],
+        //             ['redo', ['redo']],
+        //         ],
+        //         fontNames: ['Arial', 'Arial Black', 'Comic Sans MS', 'Courier New', 'Merriweather', 'Times New Roman'],
+        //         fontNamesIgnoreCheck: ['Merriweather'],
+        //         lang: 'vi-VN',
+        //         callbacks: {
+        //             onChange: (contents) => {
+        //                 this.$emit('content', contents);
+        //             }
+        //         }
+        //     });
         // },
+        // // save() {
+        // //     var markup = $(this.$refs.summernote).summernote('code');
+        // //     $(this.$refs.summernote).summernote('destroy');
+        // // },
     },
-    beforeDestroy() {
-        $(this.$refs.summernote).summernote('destroy');
-    }
+    // beforeDestroy() {
+    //     $(this.$refs.summernote).summernote('destroy');
+    // }
 };
 </script>
 
