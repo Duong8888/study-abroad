e2340e935e2d5451e4bbf88639829d77e4d46de6
duong8888 <duongna16032003@gmail.com>
2024-06-25 21:23:01 +0700

    update 25/6

--- resources/js/components/Introduce.vue
+++ resources/js/components/Introduce.vue
@@ -16,7 +16,8 @@
                         <h2 class="title pb-30">
                             Du học Hàn Quốc cùng <span class="" style="color: #ed1e24">SMARTEDU</span>
                         </h2>
-                        <p class="margin-0 pb-55 md-pb-30 text-custom">SMARTEDU được thành lập từ năm 2020, Công ty có trụ chính tại
+                        <p class="margin-0 pb-55 md-pb-30 text-custom">SMARTEDU được thành lập từ năm 2020, Công ty có
+                            trụ chính tại
                             Seoul(Hàn Quốc) và có 2 chi nhánh tại Ansan(HÀn Quốc), Hà Nội. Chỉ sau sau vài năm hoạt
                             động, Văn phòng đã có khá nhiều thành tích đáng kể như: kí hợp đồng liên kết với khoảng 10
                             trường đại học lớn tại Hàn Quốc, liên kết được với 5 trường đại học đào tạo ngôn ngữ Hàn tại
@@ -45,7 +46,8 @@
                                 <h3 class="title custom-title text-center" style="color: #ed1e24; font-weight: 900">
                                     SỨ MỆNH
                                 </h3>
-                                <p class="services-txt text-custom">Chúng tôi luôn cố gắng không ngừng và làm việc bằng cả trái tim
+                                <p class="services-txt text-custom">Chúng tôi luôn cố gắng không ngừng và làm việc bằng
+                                    cả trái tim
                                     của mình để mang lại những giá trị và hiệu quả cho khách hàng trong suốt quá trình
                                     học tập ở Smart Edu và cả sau khi sang Hàn Quốc học tập và làm việc.</p>
                             </div>
@@ -60,9 +62,53 @@
                                 <h3 class="title custom-title text-center" style="color: #ed1e24; font-weight: 900">
                                     TẦM NHÌN
                                 </h3>
-                                <p class="services-txt text-custom">Chúng tôi luôn cam kết thực hiện những điều chúng tôi nói với
+                                <p class="services-txt text-custom">Chúng tôi luôn cam kết thực hiện những điều chúng
+                                    tôi nói với
                                     khách hàng. Chúng tôi cam kết đồng hành cùng phụ huynh, học sinh để đạt mục tiêu đề
                                     ra về thành tích học tập và có được tấm vé sang Hàn Quốc.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row md-pl-15">
+                    <div class="col-lg-3 col-md-6 mb-40">
+                        <div class="services-item">
+                            <div class="services-text">
+                                <h4 class="title custom-title" style="color: #ed1e24; font-weight: 900">
+                                    <span>01. </span>Điều Kiện
+                                </h4>
+                                <p class="services-txt text-custom">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, sed!</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 mb-40">
+                        <div class="services-item">
+                            <div class="services-text">
+                                <h4 class="title custom-title" style="color: #ed1e24; font-weight: 900">
+                                    <span>02. </span>Chọn Trường
+                                </h4>
+                                <p class="services-txt text-custom">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, error.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 mb-40">
+                        <div class="services-item">
+                            <div class="services-text">
+                                <h4 class="title custom-title" style="color: #ed1e24; font-weight: 900">
+                                    <span>03. </span>Chi Phí
+                                </h4>
+                                <p class="services-txt text-custom">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae, in?</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 mb-40">
+                        <div class="services-item">
+                            <div class="services-text">
+                                <h4 class="title custom-title" style="color: #ed1e24; font-weight: 900">
+                                    <span>04. </span>Học Bổng
+                                </h4>
+                                <p class="services-txt text-custom">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, nam.</p>
                             </div>
                         </div>
                     </div>
@@ -81,14 +127,16 @@ export default {
 </script>
 
 <style scoped>
-.custom-title{
+.custom-title {
     position: relative;
+    text-transform: uppercase;
 }
-.custom-title:before{
+
+.custom-title:before {
     content: "";
     width: 20px;
     height: 100%;
-    background: rgba(246,247,249,0.5);
+    background: rgba(246, 247, 249, 0.5);
     position: absolute;
     left: -20px;
     transform: skew(40deg);
@@ -96,17 +144,20 @@ export default {
 }
 
 @keyframes animationText {
-    0%{
+    0% {
         left: -30px;
     }
-    50%,100% {
+    50%, 100% {
         left: 100%;
     }
 }
-.icon-title{
+
+.icon-title {
     width: 100px;
     height: 100px;
 }
-.text-custom{
+
+.custom-title > span {
+    font-size: 25px;
 }
 </style>
