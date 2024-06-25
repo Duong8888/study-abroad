0770824cfdcd28acd81de5e3b21714e1a6b55870
duong8888 <duongna16032003@gmail.com>
2024-06-25 16:22:01 +0700

    update 25/6

--- resources/js/views/admin/PostType.vue
+++ resources/js/views/admin/PostType.vue
@@ -54,7 +54,7 @@
                                 <input type="text" class="form-control" id="type_name" v-model="editType.type_name"
                                        required>
                                 <label for="status" class="mt-20">
-                                    <input type="checkbox" id="status" v-model="editType.status"  v-bind:checked="editType.status">
+                                    <input type="checkbox" id="status" v-model="editType.status" :data-test="editType.status"  v-bind:checked="editType.status">
                                     Hiển thị mục này ở trang chủ
                                 </label>
                             </div>
