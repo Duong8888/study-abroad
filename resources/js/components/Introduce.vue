5028258e2054cbc70d4fee5f641bdea8222dd8e6
duong8888 <duongna16032003@gmail.com>
2024-06-27 23:58:22 +0700

    update 27/6

--- resources/js/components/Introduce.vue
+++ resources/js/components/Introduce.vue
@@ -1,99 +1,130 @@
 <template>
-    <div class="outer-circle">
-        <div class="inner-circle">
-            <div class="center text-center">Thôn tin du học sinh cần biết</div>
-            <div class="quadrant quadrant1">
-                <div class="box c-1">
-                </div>
-                <div class="content">
-                    <div class="text">ĐIỀU KIỆN</div>
-                    <div class="icon">Icon1</div>
-                </div>
-            </div>
-            <div class="quadrant quadrant2">
-                <div class="box c-2">
-                </div>
-                <div class="content">
-                    <div class="text">CHỌN TRƯỜNG</div>
-                    <div class="icon">Icon2</div>
-                </div>
-            </div>
-            <div class="quadrant quadrant3">
-                <div class="box c-3">
-                </div>
-                <div class="content">
-                    <div class="text">CHI PHÍ</div>
-                    <div class="icon">Icon3</div>
-                </div>
-            </div>
-            <div class="quadrant quadrant4">
-                <div class="box c-4">
-                </div>
-                <div class="content">
-                    <div class="text">HỌC BỔNG</div>
-                    <div class="icon">Icon4</div>
+    <div class="container mb-80 md-mb-0">
+        <h2 class="title pb-30" style="font-weight: 900">
+            <span class="" style="color: #ed1e24">SMARTEDU</span> Có Gì ?
+        </h2>
+        <div class="d-flex align-items-center justify-content-center w-100">
+            <div class="outer-circle">
+                <div class="inner-circle">
+                    <div class="center text-center" style="font-weight: 900">Thôn tin du học sinh cần biết</div>
+                    <div class="quadrant quadrant1">
+                        <div class="box c-1">
+                        </div>
+                        <div class="content">
+                            <a href="#">
+                                <div class="text">
+                                    <img src="@/assets/images/common/text-3.svg" alt="logo">
+                                </div>
+                                <div class="icon-box">
+                                    <img src="@/assets/images/common/43.svg" alt="logo">
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="quadrant quadrant2">
+                        <div class="box c-2">
+                        </div>
+                        <div class="content">
+                            <a href="#">
+                                <div class="text">
+                                    <img src="@/assets/images/common/text-1.svg" alt="logo">
+                                </div>
+                                <div class="icon-box">
+                                    <img src="@/assets/images/common/44.svg" alt="logo">
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="quadrant quadrant3">
+                        <div class="box c-3">
+                        </div>
+                        <div class="content">
+                            <a href="#">
+                                <div class="text">
+                                    <img src="@/assets/images/common/text-4.svg" alt="logo">
+                                </div>
+                                <div class="icon-box">
+                                    <img src="@/assets/images/common/42.svg" alt="logo">
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="quadrant quadrant4">
+                        <div class="box c-4">
+                        </div>
+                        <div class="content">
+                            <a href="#">
+                                <div class="text">
+                                    <img src="@/assets/images/common/text-3.svg" alt="logo">
+                                </div>
+                                <div class="icon-box">
+                                    <img src="@/assets/images/common/41.svg" alt="logo">
+                                </div>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
     <!-- About Section Start -->
     <div id="about"></div>
-    <div id="rs-about" class="rs-about style1 md-pt-80  gray-bg2">
+    <div id="rs-about" class="container rs-about style1 md-pt-80 gray-bg2">
         <div class="container">
-            <div class="row y-middle">
-                <div class="sec-title text-center mb-60 w-100">
-                    <h2 class="title title2 pb-13">
-                        Thông tin du học bạn cần biết
-                    </h2>
-                    <div class="heading-border-line"></div>
-                </div>
-                <div class="row md-pl-15">
-                    <div class="col-lg-3 col-md-6 mb-40">
-                        <div class="services-item">
-                            <div class="services-text">
-                                <p class="title custom-title" style="color: #ed1e24; font-weight: 900">
-                                    <span>01. </span>Điều Kiện
-                                </p>
-                                <p class="services-txt text-custom">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, sed!</p>
-                                <a href="https://smartedu.com.vn/blogs">Xem thêm</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 mb-40">
-                        <div class="services-item">
-                            <div class="services-text">
-                                <p class="title custom-title" style="color: #ed1e24; font-weight: 900">
-                                    <span>02. </span>Chọn Trường
-                                </p>
-                                <p class="services-txt text-custom">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, error.</p>
-                                <a href="https://smartedu.com.vn/blogs">Xem thêm</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 mb-40">
-                        <div class="services-item">
-                            <div class="services-text">
-                                <p class="title custom-title" style="color: #ed1e24; font-weight: 900">
-                                    <span>03. </span>Chi Phí
-                                </p>
-                                <p class="services-txt text-custom">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae, in?</p>
-                                <a href="https://smartedu.com.vn/blogs">Xem thêm</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 mb-40">
-                        <div class="services-item">
-                            <div class="services-text">
-                                <p class="title custom-title" style="color: #ed1e24; font-weight: 900">
-                                    <span>04. </span>Học Bổng
-                                </p>
-                                <p class="services-txt text-custom">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, nam.</p>
-                                <a href="https://smartedu.com.vn/blogs">Xem thêm</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+<!--            <div class="row y-middle">-->
+<!--                <div class="sec-title text-center mb-60 w-100">-->
+<!--                    <h2 class="title title2 pb-13">-->
+<!--                        Thông tin du học bạn cần biết-->
+<!--                    </h2>-->
+<!--                    <div class="heading-border-line"></div>-->
+<!--                </div>-->
+<!--                <div class="row md-pl-15">-->
+<!--                    <div class="col-lg-3 col-md-6 mb-40">-->
+<!--                        <div class="services-item">-->
+<!--                            <div class="services-text">-->
+<!--                                <p class="title custom-title" style="color: #ed1e24; font-weight: 900">-->
+<!--                                    <span>01. </span>Điều Kiện-->
+<!--                                </p>-->
+<!--                                <p class="services-txt text-custom">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, sed!</p>-->
+<!--                                <a href="https://smartedu.com.vn/blogs">Xem thêm</a>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                    <div class="col-lg-3 col-md-6 mb-40">-->
+<!--                        <div class="services-item">-->
+<!--                            <div class="services-text">-->
+<!--                                <p class="title custom-title" style="color: #ed1e24; font-weight: 900">-->
+<!--                                    <span>02. </span>Chọn Trường-->
+<!--                                </p>-->
+<!--                                <p class="services-txt text-custom">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, error.</p>-->
+<!--                                <a href="https://smartedu.com.vn/blogs">Xem thêm</a>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                    <div class="col-lg-3 col-md-6 mb-40">-->
+<!--                        <div class="services-item">-->
+<!--                            <div class="services-text">-->
+<!--                                <p class="title custom-title" style="color: #ed1e24; font-weight: 900">-->
+<!--                                    <span>03. </span>Chi Phí-->
+<!--                                </p>-->
+<!--                                <p class="services-txt text-custom">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae, in?</p>-->
+<!--                                <a href="https://smartedu.com.vn/blogs">Xem thêm</a>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                    <div class="col-lg-3 col-md-6 mb-40">-->
+<!--                        <div class="services-item">-->
+<!--                            <div class="services-text">-->
+<!--                                <p class="title custom-title" style="color: #ed1e24; font-weight: 900">-->
+<!--                                    <span>04. </span>Học Bổng-->
+<!--                                </p>-->
+<!--                                <p class="services-txt text-custom">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, nam.</p>-->
+<!--                                <a href="https://smartedu.com.vn/blogs">Xem thêm</a>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
 
             <div class="row y-middle">
                 <div class="col-lg-6 md-mb-50">
@@ -214,8 +245,8 @@ export default {
 
 .outer-circle {
     position: relative;
-    width: 400px;
-    height: 400px;
+    width: 380px;
+    height: 380px;
     border-radius: 50%;
     transform: rotate(45deg);
 }
@@ -231,8 +262,8 @@ export default {
 
 .center {
     position: absolute;
-    width: 100px;
-    height: 100px;
+    width: 120px;
+    height: 120px;
     background-color: #EDABAC;
     border-radius: 50%;
     display: flex;
@@ -240,6 +271,20 @@ export default {
     align-items: center;
     z-index: 2;
     transform: rotate(-45deg);
+    font-weight: bold;
+    padding: 15px;
+}
+.center::before{
+    content: "";
+    position: absolute;
+    width: 20px;
+    height: 20px;
+    background: #EDABAC;
+    /*rotate: 160deg;*/
+    left: 0;
+    top: 0;
+    transform: translateY(50%) translateX(50%);
+    z-index: -1;
 }
 
 .quadrant {
@@ -314,10 +359,15 @@ export default {
     transform: rotate(135deg);
 }
 
-.icon {
+.icon-box {
     font-size: 24px;
-    margin-bottom: 10px;
     z-index: 3;
+}
+
+.icon-box img{
+    transform: translateY(10px);
+    width: 40px;
+    height: 40px;
 }
 
 .text {
@@ -329,7 +379,8 @@ export default {
 }
 
 .quadrant:hover {
-    transform: scale(1.4);
+    transform: scale(1.3);
+    cursor: pointer;
 }
 
 .box{
@@ -363,5 +414,23 @@ export default {
     top: 0;
     left: 0;
     border-bottom-right-radius: 100%;
+}
+
+@media only screen and (max-width: 575px){
+    .quadrant:hover {
+        transform: scale(1.1);
+    }
+    .inner-circle{
+        transform: scale(0.8);
+    }
+    .outer-circle {
+        width: 300px;
+        height: 300px;
+    }
+    .icon-box img{
+        transform: translateY(5px);
+        width: 30px;
+        height: 30px;
+    }
 }
 </style>
